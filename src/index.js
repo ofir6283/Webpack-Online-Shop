9fa6fec5122cdd3556bab1bdc39ca9e4bd61c186
ofir6283 <93862727+ofir6283@users.noreply.github.com>
2022-12-27 17:54:15 +0200

    webpack and files added.

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { items } from './items';
+
+console.log(items);
---
+++ src/items.js
@@ -0,0 +1,16 @@
+const items = [
+    {
+        "id": 1,
+        "name": "Kiwi",
+        "price": 1.90,
+        "image": "TOADD: IMAGE",
+    },
+    {
+        "id": 2,
+        "name": "Appricote",
+        "price": 3.90,
+        "image": "TOADD: IMAGE",
+    }
+]
+export { items };
+
