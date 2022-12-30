1c9226ff8a62afffca1217bf9f79831ea1217e81
ofir6283 <ofir6283@gmail.com>
2022-12-30 12:54:58 +0200

    search complete

--- src/init.js
+++ src/init.js
@@ -2,10 +2,27 @@
 import { items } from "./items";
 import { buildProdCard } from "./productCard";
 
-let cat = document.getElementById('cat');
-for (let i = 0; i < items.length; i++) {
-    buildProdCard(i);
+
+const categorySelection = document.getElementById("category");
+categorySelection.addEventListener("change", () => getItemsByCat(items));
+
+let relevantProd = [];
+function getItemsByCat(items) {
+    relevantProd.splice(0, relevantProd.length);
+    const category = document.getElementById('category').value;
+
+    items.map((item) => {
+        if (item.category === category) {
+            relevantProd.push(item);
+        }
+    });
+    console.log(relevantProd);
+    let cat = document.getElementById('cat');
+    for (let i = 0; i < items.length; i++) {
+        buildProdCard(i, relevantProd);
+    }
 }
+
 
 
 
