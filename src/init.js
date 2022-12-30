c91403450b52c6cf1bc2319cdbd1412f327636ad
ofir6283 <ofir6283@gmail.com>
2022-12-30 14:54:40 +0200

    finished without total cart price.

--- src/init.js
+++ src/init.js
@@ -1,20 +1,24 @@
-
 import { items } from "./items";
 import { buildProdCard } from "./productCard";
 
-let relavntArray = []
-const categorySelect = document.getElementById("categories");
-categorySelect.addEventListener("change", () => getRelevantProducts(items));
-function getRelevantProducts(products) {
 
-    document.getElementById("cat").value;
+const categorySelection = document.getElementById("category");
+categorySelection.addEventListener("change", () => getItemsByCat(items));
+
+let relevantProd = [];
+function getItemsByCat(items) {
+    document.getElementById("cat").replaceChildren();
+    relevantProd.splice(0, relevantProd.length);
+    const category = document.getElementById('category').value;
+
+    items.map((item) => {
+        if (item.category === category) {
+            relevantProd.push(item);
+        }
+    });
+    console.log(relevantProd);
     let cat = document.getElementById('cat');
     for (let i = 0; i < items.length; i++) {
         buildProdCard(i, relevantProd);
     }
 }
-
-
-
-
-
--- src/productCard.js
+++ src/productCard.js
@@ -1,21 +1,20 @@
-
 import { items } from "./items";
 
 function buildProdCard(i, relevantProd) {
     let div = document.createElement('div');
     div.setAttribute('style', 'border: 0.2em solid black;margin:1em;font-family:Times New Roman-serif;height: 15em;width: 8em;border-radius: 15px; text-align: center;background-color: lightgreen;');
-
+    // div.classList.add('catalog-item');
     let img = document.createElement('img');
     img.setAttribute('style', 'width:3em', 'height:3em');
-    img.src = items[i].image;
+    img.src = relevantProd[i].image;
     img.setAttribute('style', 'width:3em;height:3em');
 
     let title = document.createElement('p');
-    title.innerHTML = items[i].name;
+    title.innerHTML = relevantProd[i].name;
     title.setAttribute('style', 'size:0.5em;font-weight: bold;')
 
     let price = document.createElement('p');
-    price.innerHTML = "price:" + items[i].price + " shmekels";
+    price.innerHTML = "price:" + relevantProd[i].price + " shmekels";
     price.setAttribute('style', 'text-decoration: underline;');
 
     let productCountInput = document.createElement('input');
@@ -26,23 +25,24 @@ function buildProdCard(i, relevantProd) {
     orderButton.innerHTML = "add to cart";
     orderButton.setAttribute('style', 'margin-top: 2em;text-align:center;border:none;border-radius: 5px;cursor: pointer;display: inline-block;text-align: center;color:#A9A9A9;border-color: #f8f8ff;');
 
+    // Order Button logic
     orderButton.addEventListener('click', () => {
-        console.log("INSIDE ADD EVENT");
+
         let cart = document.getElementById('cart');
         let cartDiv = document.createElement('div');
         cartDiv.setAttribute('style', 'border: 0.2em solid black;margin:1em;font-family:Times New Roman-serif;height: 15em;width: 8em;border-radius: 15px; text-align: center;background-color: lightgreen;');
         let img = document.createElement('img');
         img.setAttribute('style', 'width:3em', 'height:3em');
-        img.src = items[i].image;
+        img.src = relevantProd[i].image;
         img.setAttribute('style', 'width:3em;height:3em');
 
         let title = document.createElement('p');
-        title.innerHTML = items[i].name + " X " + Number(productCountInput.value);
+        title.innerHTML = relevantProd[i].name + " X " + Number(productCountInput.value);
         title.setAttribute('style', 'size:0.5em')
 
         let price = document.createElement('p');
         price.setAttribute('style', 'text-decoration: underline');
-        price.innerHTML = "price:" + items[i].price + " shmekels";
+        price.innerHTML = "price:" + relevantProd[i].price + " shmekels";
 
 
 
@@ -50,6 +50,7 @@ function buildProdCard(i, relevantProd) {
         let removeBtn = document.createElement('button');
         removeBtn.innerHTML = "Remove";
         removeBtn.setAttribute('style', 'margin-top: 2em;width:4.3em;height:2em;text-align:center;border:none;border-radius: 5px;cursor: pointer;display: inline-block;text-align: center;color:#A9A9A9;border-color: #f8f8ff;');
+
 
         removeBtn.addEventListener('click', () => {
             removeFromCart(cartDiv);
@@ -88,8 +89,9 @@ function removeFromCart(cartDiv) {
     cartDiv.remove();
 }
 
-function searchByCatagory() {
-    //FIll
+
+function totalCartPrice() {
+
 }
 
 export { buildProdCard };
